9a5a4e2c4fff206c011b0fec0936680d5baa0c98
suhail kandanur <ksuhail7@gmail.com>
2016-11-25 17:28:28 -0500

    merge sort on an array of integers problem 11.1

--- src/main/java/com/suhailkandanur/eopi/ch11/MergeSortedFilesP11_1.java
+++ src/main/java/com/suhailkandanur/eopi/ch11/MergeSortedFilesP11_1.java
@@ -1,6 +1,8 @@
 package com.suhailkandanur.eopi.ch11;
 
 import com.suhailkandanur.eopi.util.RandomGen;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Problem: 11.1 - Design an algorithm that takes as set of files containing stock trades sorted by increasing trade times,
@@ -10,45 +12,74 @@ import com.suhailkandanur.eopi.util.RandomGen;
  * Created by suhail on 2016-11-25.
  */
 public class MergeSortedFilesP11_1 {
+    private static final Logger logger = LoggerFactory.getLogger(MergeSortedFilesP11_1.class);
     //this will implement merge sort on a array of integers
 
-    public static int[] mergeSort(int[] unsorted) {
-        if(unsorted == null || unsorted.length < 1)
+    public static int[] mergeSort(final int[] unsorted) {
+        if (unsorted == null || unsorted.length < 1)
             return unsorted;
         int[] sortedArray = new int[unsorted.length];
-        sort(unsorted, sortedArray, 0, unsorted.length -1);
+        //copy elements into sorted array
+        for (int i = 0; i < unsorted.length; i++) {
+            sortedArray[i] = unsorted[i];
+        }
+        sort(sortedArray, 0, unsorted.length - 1);
         return sortedArray;
     }
 
-    public static void sort(int[] actualArray, int[] sortedArray, int lo, int hi) {
-        if(lo >= hi) {
+    public static void sort(int[] actualArray, int lo, int hi) {
+        if (lo >= hi) {
             return;
         }
 
-        int mid = lo + ((hi - lo)/2);
-
-        sort(actualArray, sortedArray, lo, mid);
-        sort(actualArray, sortedArray, mid + 1, hi);
-        merge(actualArray, sortedArray, lo, mid, hi);
+        int mid = lo + ((hi - lo) / 2);
+        //logger.info("sorting (lo: {}, mid: {})", lo, mid);
+        sort(actualArray, lo, mid);
+        //logger.info("sorting (mid + 1: {}, hi; {})" , mid + 1 , hi);
+        sort(actualArray, mid + 1, hi);
+//        logger.info("Merging (lo: {}, mid: {}, hi: {})", lo, mid, hi);
+        merge(actualArray, lo, mid, hi);
     }
 
-    public static void merge(int [] actualArray, int[] sortedArray, int lo, int mid, int hi) {
+    public static void merge(int[] actualArray, int lo, int mid, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+        int len = hi - lo + 1;
+        int[] temp = new int[len];
         int i = lo;
         int j = mid + 1;
+        int count = 0;
         while (i <= mid && j <= hi) {
-
+            if (actualArray[i] >= actualArray[j]) {
+                temp[count] = actualArray[j++];
+            } else {
+                temp[count] = actualArray[i++];
+            }
+            count++;
+        }
+        while (i <= mid) temp[count++] = actualArray[i++];
+        while (j <= hi) temp[count++] = actualArray[j++];
+        //copy into actualArray
+        for (int k = 0; k < len; k++) {
+            actualArray[lo + k] = temp[k];
         }
     }
 
     public static void main(String[] args) {
-        int[] unsortedIntArray = RandomGen.generateRandom(100);
-        for (int i = 0; i < unsortedIntArray.length; i++) {
-            System.out.println(unsortedIntArray[i]);
-        }
-
+        int[] unsortedIntArray = RandomGen.generateRandom(20, 200);
+        printArray(unsortedIntArray);
         int[] sortedArray = mergeSort(unsortedIntArray);
-        for (int i = 0; i < sortedArray.length; i++) {
-            System.out.println(sortedArray[i]);
+        printArray(sortedArray);
+    }
+
+    private static void printArray(int[] array) {
+        if (array == null || array.length < 1) {
+            return;
         }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println();
     }
 }
