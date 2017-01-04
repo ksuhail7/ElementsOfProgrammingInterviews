872e2be381c10d42a572a88b8793bffe79a3604f
suhail kandanur <ksuhail7@live.com>
2017-01-04 17:03:29 -0500

    kth largest element in an array

--- python/KthLargestNumber.py
+++ python/KthLargestNumber.py
@@ -6,7 +6,7 @@ import random
 lo to large-1 : numbers > k
 large to equals - 1: numbers == k
 equals to small: unknown
-small+1 to hi : numbers > k
+small+1 to hi : numbers < k
 """
 def partition_array(array, lo, hi, k):
 	split = -1
@@ -19,29 +19,48 @@ def partition_array(array, lo, hi, k):
 
 	while equals <= small:
 		if array[equals] > k:
-			array[equals] = array[large] + array[equals]
-			array[large] = array[equals] - array[large]
-			array[equals] = array[equals] - array[large]
+			temp = array[large]
+			array[large] = array[equals]
+			array[equals] = temp
 			equals+=1
 			large+=1
 		elif array[equals] == k:
 			equals+=1
 		elif array[equals] < k:
-			array[equals] = array[small] + array[equals]
-			array[large] = array[equals] - array[small]
-			array[equals] = array[equals] - array[small]
-			equals+=1
+			temp = array[small]
+			array[small] = array[equals]
+			array[equals] = temp
 			small-=1
 
+	return large
 
-
+def find_k_th_largest(array, lo, hi, k):
+	print(array, "lo", lo, "hi", hi, "k", k)
+	if lo > hi:
+		return -1
+	random_k = random.randint(lo, hi)
+	print("random", array[random_k])
+	split = partition_array(array, lo, hi, array[random_k])
+	print("split", split)
+	if split+1 == k:
+		return split
+	elif split+1 > k:
+		return find_k_th_largest(array, lo, split - 1, k)
+	elif split+1 < k:
+		#split = 1
+		#k = 3
+		#lo = k - split
+		return find_k_th_largest(array, split + 1, hi, k)
 
 size = 10
 min = 0
 max = 100
 
 array = [random.randint(0, 100) for x in range(0, size)]
-
+arr_length = len(array)
 k = 3
 
 print(array)
+kth = find_k_th_largest(array, 0, len(array)-1, k)
+print(array, "kth", kth)
+print("kth largest", array[kth])
