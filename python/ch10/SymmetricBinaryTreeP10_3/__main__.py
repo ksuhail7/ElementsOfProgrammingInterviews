74bc231b013df52da5871ad4a91ec0a8a45f3807
ksuhail7 <ksuhail7@gmail.com>
2017-01-14 19:27:16 -0500

    solution to problem 10.3 - symmetric binary tree

---
+++ python/ch10/SymmetricBinaryTreeP10_3/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from tree import BinaryTree
+import random
+
+# array = [random.randint(0, 100) for x in range(0, 10)]
+array = [314, 6, 6, -1, 2, 2, -1, -1, -1, -1, 3, 3, -1]
+print(array)
+bst = BinaryTree(array)
+# bst.inorder_traversal()
+print(bst.is_symmetric())
